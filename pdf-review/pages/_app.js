f930cb2f184906b7f02d9188b1bef5060ace8f67
Marco Segreto <msegreto@ideo.com>
2020-04-13 22:49:50 -0700

    Start on some basic styles

--- pdf-review/pages/_app.js
+++ pdf-review/pages/_app.js
@@ -1,9 +1,19 @@
 import '../style.scss'
+import { Container } from 'next/app';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <main>
-      <Component {...pageProps} />
-    </main>
+    <Container>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,300;0,400;1,300&family=Oswald:wght@400;500&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </Container>
   )
 }
